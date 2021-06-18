7455ee0be6ebd020a367f4fdfc41c64f602a61c8
Romualdo Santiago Santiago <malbe_11@hotmail.com>
2021-06-18 13:44:26 -0500

    php artisan make:model Color -mfr --api

---
+++ database/factories/ColorFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Color;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ColorFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Color::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => 'sand dollar',
+            'color' => '#DECDBE',
+            'pantone' => '13-1106',
+            'year' => '2006',
+        ];
+    }
+}
